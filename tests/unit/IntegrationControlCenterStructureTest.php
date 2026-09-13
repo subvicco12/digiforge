d1a673ae150e26773ba5daa2c22327099c71017d
subvicco12 <subvicco12@gmail.com>
2026-09-13 20:34:51 +0530

    Fix standards warning in connection control test

--- tests/unit/IntegrationControlCenterStructureTest.php
+++ tests/unit/IntegrationControlCenterStructureTest.php
@@ -65,12 +65,14 @@ final class IntegrationControlCenterStructureTest extends TestCase
         $admin = (string) file_get_contents(__DIR__ . '/../../includes/Integrations/Admin.php');
         $repository = (string) file_get_contents(__DIR__ . '/../../includes/Integrations/Repository.php');
         $tester = (string) file_get_contents(__DIR__ . '/../../includes/Integrations/ConnectionTester.php');
+        $adminMigration = "migrateSecretName(\$id, 'personal_access_token', 'api_key')";
+        $testerMigration = "migrateSecretName(\$integrationId, 'personal_access_token', 'api_key')";
 
         self::assertStringContainsString('admin_post_digiforge_gelato_normalize_key', $admin);
         self::assertStringContainsString('Repair Gelato key label', $admin);
-        self::assertStringContainsString("migrateSecretName(\$id, 'personal_access_token', 'api_key')", $admin);
+        self::assertStringContainsString($adminMigration, $admin);
         self::assertStringContainsString('migrateSecretName', $repository);
-        self::assertStringContainsString("migrateSecretName(\$integrationId, 'personal_access_token', 'api_key')", $tester);
+        self::assertStringContainsString($testerMigration, $tester);
     }
 
     public function testCredentialVaultCanProvisionEncryptedManagedMasterKey(): void
