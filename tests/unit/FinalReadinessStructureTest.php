326c19e36f30730ad77adb69daafeab00f0cf7e2
subvicco12 <subvicco12@gmail.com>
2026-09-13 13:43:35 +0530

    Test WordPress recovery option boolean parsing

--- tests/unit/FinalReadinessStructureTest.php
+++ tests/unit/FinalReadinessStructureTest.php
@@ -17,6 +17,7 @@ final class FinalReadinessStructureTest extends TestCase
         self::assertStringContainsString('digiforge_recovery_plugin_package_available', $source);
         self::assertStringContainsString('digiforge_recovery_checksum_verified', $source);
         self::assertStringContainsString('digiforge_recovery_restore_instructions_available', $source);
+        self::assertStringContainsString('FILTER_VALIDATE_BOOLEAN', $source);
         self::assertStringContainsString("'external_actions_performed' => false", $source);
     }
 
