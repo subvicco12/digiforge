86932e50f1f9f0f872e92d52f07cfd7006625bf7
subvicco12 <subvicco12@gmail.com>
2026-09-15 00:17:08 +0530

    U3: fix scheduler assertion literal

--- tests/unit/U3ProductFactoryStructureTest.php
+++ tests/unit/U3ProductFactoryStructureTest.php
@@ -65,7 +65,7 @@ final class U3ProductFactoryStructureTest extends TestCase
     public function testApprovalAutomationChecksSchedulerResult(): void
     {
         $source = (string) file_get_contents(__DIR__ . '/../../includes/ProductFactory/ApprovalAutomation.php');
-        self::assertStringContainsString("$scheduled = false", $source);
+        self::assertStringContainsString('$scheduled = false', $source);
         self::assertStringContainsString('scheduler_rejected_job', $source);
         self::assertStringContainsString("'research_candidate_reviewed'", $source);
     }
