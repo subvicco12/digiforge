7c8b82f94bb9f52ca4fdf69438a83562dd2fd092
subvicco12 <subvicco12@gmail.com>
2026-09-16 20:49:43 +0530

    CI: certify repair ZIP identity through repair variant

--- tests/unit/U3RepairPackageIdentityStructureTest.php
+++ tests/unit/U3RepairPackageIdentityStructureTest.php
@@ -10,11 +10,11 @@ final class U3RepairPackageIdentityStructureTest extends TestCase
     {
         $source = file_get_contents(dirname(__DIR__, 2) . '/includes/ProductFactory/Orchestrator.php');
         self::assertIsString($source);
+        self::assertStringContainsString("\$repairVariant=\$isRepair?'repair-'.\$planToken:''", $source);
         self::assertStringContainsString('$packageFilename=$isRepair?', $source);
-        self::assertStringContainsString("'-repair-'.\$planToken.'.zip'", $source);
-        self::assertStringContainsString('$packageVariant=$isRepair?', $source);
+        self::assertStringContainsString("'-'.\$repairVariant.'.zip'", $source);
         self::assertStringContainsString("'variant_key'=>\$variant", $source);
-        self::assertStringContainsString('registerPackage($productVersionId,(int)$plan[\'id\'],$package,$key,$sequence,$packageVariant)', $source);
+        self::assertStringContainsString('registerPackage($productVersionId,(int)$plan[\'id\'],$package,$key,$sequence,$repairVariant)', $source);
         self::assertStringContainsString("'external_publish'=>false", $source);
     }
 }
