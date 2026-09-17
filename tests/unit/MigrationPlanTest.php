4136213f8f856f15a4eca22e0a8cead42c7cb2bc
subvicco12 <subvicco12@gmail.com>
2026-09-17 11:33:16 +0530

    Extend migration plan tests through schema v14

--- tests/unit/MigrationPlanTest.php
+++ tests/unit/MigrationPlanTest.php
@@ -11,17 +11,18 @@ final class MigrationPlanTest extends TestCase
 {
     public function testPendingMigrationsAreOrderedAndResumable(): void
     {
-        self::assertSame([1,2,3,4,5,6,7,8,9,10,11,12,13], MigrationPlan::pending(0));
-        self::assertSame([3,4,5,6,7,8,9,10,11,12,13], MigrationPlan::pending(2));
-        self::assertSame([5,6,7,8,9,10,11,12,13], MigrationPlan::pending(4));
-        self::assertSame([6,7,8,9,10,11,12,13], MigrationPlan::pending(5));
-        self::assertSame([7,8,9,10,11,12,13], MigrationPlan::pending(6));
-        self::assertSame([8,9,10,11,12,13], MigrationPlan::pending(7));
-        self::assertSame([9,10,11,12,13], MigrationPlan::pending(8));
-        self::assertSame([10,11,12,13], MigrationPlan::pending(9));
-        self::assertSame([11,12,13], MigrationPlan::pending(10));
-        self::assertSame([12,13], MigrationPlan::pending(11));
-        self::assertSame([13], MigrationPlan::pending(12));
-        self::assertSame([], MigrationPlan::pending(13));
+        self::assertSame([1,2,3,4,5,6,7,8,9,10,11,12,13,14], MigrationPlan::pending(0));
+        self::assertSame([3,4,5,6,7,8,9,10,11,12,13,14], MigrationPlan::pending(2));
+        self::assertSame([5,6,7,8,9,10,11,12,13,14], MigrationPlan::pending(4));
+        self::assertSame([6,7,8,9,10,11,12,13,14], MigrationPlan::pending(5));
+        self::assertSame([7,8,9,10,11,12,13,14], MigrationPlan::pending(6));
+        self::assertSame([8,9,10,11,12,13,14], MigrationPlan::pending(7));
+        self::assertSame([9,10,11,12,13,14], MigrationPlan::pending(8));
+        self::assertSame([10,11,12,13,14], MigrationPlan::pending(9));
+        self::assertSame([11,12,13,14], MigrationPlan::pending(10));
+        self::assertSame([12,13,14], MigrationPlan::pending(11));
+        self::assertSame([13,14], MigrationPlan::pending(12));
+        self::assertSame([14], MigrationPlan::pending(13));
+        self::assertSame([], MigrationPlan::pending(14));
     }
 }
