99bcfcb12c18e64f3853e622310cf9d13f62cc71
subvicco12 <subvicco12@gmail.com>
2026-09-18 00:27:03 +0530

    Align admin policy tests with canonical scope enforcement

--- tests/unit/PodAdminScopePolicyStructureTest.php
+++ tests/unit/PodAdminScopePolicyStructureTest.php
@@ -12,10 +12,10 @@ final class PodAdminScopePolicyStructureTest extends TestCase
         self::assertIsString($source);
         self::assertStringContainsString("current_user_can('manage_digiforge_pod')",$source);
         self::assertStringContainsString('BusinessScope::resolveConfigured($input)',$source);
-        self::assertStringContainsString('ORIGINAL_DESIGN_POD',$source);
-        self::assertStringContainsString('DIGICRAFTIFY_GOODS',$source);
+        self::assertStringContainsString('canonical ACTIVE ownership',$source);
+        self::assertStringContainsString('PERSONALIZED_POD-only',$source);
+        self::assertStringNotContainsString("sanitize_key((string)(\$scope['business_id']",$source);
         self::assertStringNotContainsString('wp_remote_',$source);
-        self::assertStringNotContainsString('publish',$source);
         self::assertStringNotContainsString('createOrder',$source);
     }
 }
