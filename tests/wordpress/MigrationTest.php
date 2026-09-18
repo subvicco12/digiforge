8f7d728632946b198756f9005b605dcb7c06ff20
subvicco12 <subvicco12@gmail.com>
2026-09-18 05:34:40 +0530

    Cover production template table in migration tests

--- tests/wordpress/MigrationTest.php
+++ tests/wordpress/MigrationTest.php
@@ -16,7 +16,7 @@ final class MigrationTest extends WP_UnitTestCase
     private function podTables(): array
     {
         return [
-            DigiForge\Database\Tables::pod_catalog(), DigiForge\Database\Tables::pod_mappings(), DigiForge\Database\Tables::pod_print_areas(),
+            DigiForge\Database\Tables::pod_catalog(), DigiForge\Database\Tables::pod_production_templates(), DigiForge\Database\Tables::pod_mappings(), DigiForge\Database\Tables::pod_print_areas(),
             DigiForge\Database\Tables::personalization_schemas(), DigiForge\Database\Tables::personalization_bindings(), DigiForge\Database\Tables::pod_provider_intents(),
             DigiForge\Database\Tables::pod_cost_snapshots(), DigiForge\Database\Tables::pod_readiness_reviews(),
         ];
