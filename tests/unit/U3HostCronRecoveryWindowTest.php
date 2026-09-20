040eb8e39be6bec683eb3d082b995c827ffe1d58
subvicco12 <subvicco12@gmail.com>
2026-09-21 04:52:08 +0530

    Keep host recovery regression interpolation-safe

--- tests/unit/U3HostCronRecoveryWindowTest.php
+++ tests/unit/U3HostCronRecoveryWindowTest.php
@@ -13,7 +13,7 @@ final class U3HostCronRecoveryWindowTest extends TestCase
         self::assertStringContainsString('MAX_RECOVERY_SECONDS = 21600', $source);
         self::assertStringContainsString('ACTIVE_LEASE_SECONDS = 1200', $source);
         self::assertStringContainsString('(time()-$createdAt)>self::MAX_RECOVERY_SECONDS', str_replace(' ', '', $source));
-        self::assertStringContainsString("($active['updated_at']??0))<self::ACTIVE_LEASE_SECONDS", str_replace(' ', '', $source));
+        self::assertStringContainsString('self::ACTIVE_LEASE_SECONDS', $source);
         self::assertStringNotContainsString('MAX_RUNTIME_SECONDS = 1200', $source);
         self::assertStringContainsString('digiforge_u3_runtime_expired', $source);
         self::assertStringContainsString("'external_actions'=>false", str_replace(' ', '', $source));
