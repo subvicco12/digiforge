43fdca9cb1e1e569ba3474e6a27549aac15c1ec0
subvicco12 <subvicco12@gmail.com>
2026-09-21 04:51:37 +0530

    Add regression contract for delayed host scheduler recovery

---
+++ tests/unit/U3HostCronRecoveryWindowTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class U3HostCronRecoveryWindowTest extends TestCase
+{
+    public function testRecoveryDeadlineAndActiveLeaseAreIndependent(): void
+    {
+        $source = file_get_contents(__DIR__ . '/../../includes/ProductFactory/ApprovalAutomation.php');
+        self::assertIsString($source);
+        self::assertStringContainsString('MAX_RECOVERY_SECONDS = 21600', $source);
+        self::assertStringContainsString('ACTIVE_LEASE_SECONDS = 1200', $source);
+        self::assertStringContainsString("(time()-$createdAt)>self::MAX_RECOVERY_SECONDS", str_replace(' ', '', $source));
+        self::assertStringContainsString("($active['updated_at']??0))<self::ACTIVE_LEASE_SECONDS", str_replace(' ', '', $source));
+        self::assertStringNotContainsString('MAX_RUNTIME_SECONDS = 1200', $source);
+        self::assertStringContainsString('digiforge_u3_runtime_expired', $source);
+        self::assertStringContainsString("'external_actions'=>false", str_replace(' ', '', $source));
+    }
+}
