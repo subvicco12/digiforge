82960bd8cdc0f9673bae252379afc9beb6481a29
subvicco12 <subvicco12@gmail.com>
2026-09-21 19:59:58 +0530

    Align repair package identity test with approved package naming

--- tests/unit/U3RepairPackageIdentityStructureTest.php
+++ tests/unit/U3RepairPackageIdentityStructureTest.php
@@ -11,8 +11,8 @@ final class U3RepairPackageIdentityStructureTest extends TestCase
         $source = file_get_contents(dirname(__DIR__, 2) . '/includes/ProductFactory/Orchestrator.php');
         self::assertIsString($source);
         self::assertStringContainsString("\$repairVariant=\$isRepair?'repair-'.\$planToken:''", $source);
-        self::assertStringContainsString('$packageFilename=$isRepair?', $source);
-        self::assertStringContainsString("'-'.\$repairVariant.'.zip'", $source);
+        self::assertStringContainsString('$packageFilename=$this->approvedPackageFilename($spec,$productVersionId)', $source);
+        self::assertStringContainsString('$this->producer->package($productVersionId,$packageInput,$packageFilename,$repairVariant)', $source);
         self::assertStringContainsString("'variant_key'=>\$variant", $source);
         self::assertStringContainsString('registerPackage($productVersionId,(int)$plan[\'id\'],$package,$key,$sequence,$repairVariant)', $source);
         self::assertStringContainsString("'external_publish'=>false", $source);
