9d120aa24052ded0190a5315fd56ef9b3a3942ec
subvicco12 <subvicco12@gmail.com>
2026-09-21 20:32:55 +0530

    Align v1.0.22 release assertions

--- tests/unit/ProductFactoryV1021OutputContractTest.php
+++ tests/unit/ProductFactoryV1021OutputContractTest.php
@@ -18,7 +18,7 @@ final class ProductFactoryV1021OutputContractTest extends TestCase{
   self::assertStringContainsString("['package_filename']",$orchestrator);
   self::assertStringContainsString("['delivery_archive']",$orchestrator);
   self::assertStringContainsString("['archive_filename']",$orchestrator);
-  self::assertStringContainsString("'generator_version'=>'1.0.21'",$orchestrator);
+  self::assertStringContainsString("'generator_version'=>'1.0.22'",$orchestrator);
   self::assertStringContainsString("['sha256,filename']",$orchestrator);
   self::assertStringContainsString("'external_publish'=>false",$orchestrator);
  }
