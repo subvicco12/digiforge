bb04cee62c03ef99f1bdd87f12f8d2b702fdc118
subvicco12 <subvicco12@gmail.com>
2026-09-24 19:36:08 +0530

    Add immutable POD UNKNOWN evidence schema

--- includes/Database/PodSchema.php
+++ includes/Database/PodSchema.php
@@ -65,6 +65,24 @@ final class PodSchema
     public static function statements(string $charset): array
     {
         return [
+            "CREATE TABLE " . Tables::pod_execution_unknowns() . " (
+  id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  action varchar(100) NOT NULL,
+  evidence_hash char(64) NOT NULL,
+  authorization_hash char(64) NOT NULL,
+  nonce_hash char(64) NOT NULL,
+  failure_category varchar(64) NOT NULL DEFAULT '',
+  failure_code varchar(100) NOT NULL DEFAULT '',
+  executed_by bigint(20) unsigned NOT NULL DEFAULT 0,
+  recorded_at datetime NOT NULL,
+  unknown_hash char(64) NOT NULL,
+  created_at datetime NOT NULL,
+  PRIMARY KEY  (id),
+  UNIQUE KEY authorization_hash (authorization_hash),
+  UNIQUE KEY unknown_hash (unknown_hash),
+  KEY nonce_hash (nonce_hash),
+  KEY action_recorded_at (action,recorded_at)
+) $charset;",
             "CREATE TABLE " . Tables::pod_execution_failures() . " (
   id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   action varchar(100) NOT NULL,
