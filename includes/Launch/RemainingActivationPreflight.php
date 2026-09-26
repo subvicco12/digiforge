c4f5a6805de4ee4398a02aa3cd668f9d498c2d59
subvicco12 <subvicco12@gmail.com>
2026-09-26 20:06:55 +0530

    Require Etsy Publish and a supported POD provider before orders

--- includes/Launch/RemainingActivationPreflight.php
+++ includes/Launch/RemainingActivationPreflight.php
@@ -13,7 +13,7 @@ final class RemainingActivationPreflight
         $map = [
             'gelato' => ['requires' => 'product_development', 'auth' => 'gelato_activation_authorized'],
             'etsy_publish' => ['requires' => 'etsy_draft', 'auth' => 'etsy_publish_activation_authorized'],
-            'order_automation' => ['requires' => 'printify', 'auth' => 'order_automation_activation_authorized'],
+            'order_automation' => ['requires' => 'etsy_publish', 'auth' => 'order_automation_activation_authorized'],
             'gst_automation' => ['requires' => 'order_automation', 'auth' => 'gst_automation_activation_authorized'],
         ];
         if (!isset($map[$capability])) {
@@ -26,6 +26,7 @@ final class RemainingActivationPreflight
             'activation_authorized'=>Settings::get('activation_authorized',false)===true,
             'automation_armed'=>Settings::get('automation_armed',false)===true,
             'required_capability_effective'=>Settings::is_enabled($required)===true,
+            'supported_pod_provider_effective'=>$capability!=='order_automation' || Settings::is_enabled('printify')===true || Settings::is_enabled('gelato')===true,
             'capability_configured'=>Settings::get($capability,false)===true,
             'capability_not_authorized_yet'=>Settings::get($authorizationKey,false)===false,
             'capability_not_effective_yet'=>Settings::is_enabled($capability)===false,
