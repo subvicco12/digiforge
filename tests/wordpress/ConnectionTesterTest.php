4c321351157de4b27a79597f8ce79fdfc033d9b0
subvicco12 <subvicco12@gmail.com>
2026-09-26 15:58:38 +0530

    Authorize scoped Printify in connection integration test

--- tests/wordpress/ConnectionTesterTest.php
+++ tests/wordpress/ConnectionTesterTest.php
@@ -7,6 +7,7 @@ final class ConnectionTesterTest extends WP_UnitTestCase
     public function testPrintifyReadOnlyConnectionTestUsesStoredCredentialAndRecordsSuccess(): void
     {
         DigiForge\Core\Activator::activate();
+        update_option('digiforge_settings', array_merge(DigiForge\Core\Settings::all(), ['stop_all'=>false,'activation_authorized'=>true,'automation_armed'=>true,'product_development'=>true,'product_development_activation_authorized'=>true,'printify'=>true,'printify_activation_authorized'=>true]));
         $repository = new DigiForge\Integrations\Repository();
         $created = $repository->create([
             'provider' => 'printify',
